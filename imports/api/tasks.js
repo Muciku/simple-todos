fc62cdfdf86725a10d19827869f5800cad15935b
enjogu <enjogu333@gmail.com>
2018-09-18 11:47:34 +0100

    Add publication for tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,13 @@ import { Mongo } from 'meteor/mongo';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
